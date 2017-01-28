bac316b9eb108a3d9589c0e7af77bbe0f1711ced
Ryosuke Narimoto <r-narimoto@ist.osaka-u.ac.jp>
2017-01-28 14:43:05 +0900

    waste of time

--- lib/routing_switch.rb
+++ lib/routing_switch.rb
@@ -46,13 +46,12 @@ class RoutingSwitch < Trema::Controller
     #puts packet_in.in_port
     #puts packet_in.data
     @topology.packet_in(dpid, packet_in)
+    @path_manager.packet_in(dpid, packet_in) unless packet_in.lldp?
     case packet_in.data
     when Arp::Request
       packet_in_arp_request dpid, packet_in
     when Arp::Reply
       packet_in_arp_reply dpid, packet_in
-    else
-      @path_manager.packet_in(dpid, packet_in) unless packet_in.lldp?
     end
   end
 
--- vendor/topology/lib/topology_controller.rb
+++ vendor/topology/lib/topology_controller.rb
@@ -52,11 +52,13 @@ class TopologyController < Trema::Controller
   def packet_in(dpid, packet_in)
     case packet_in.data
     when Arp::Request
+      p packet_in.sender_protocol_address
       @topology.maybe_add_host(packet_in.source_mac,
                                packet_in.sender_protocol_address,
                                dpid,
                                packet_in.in_port)
     when Arp::Reply
+      p packet_in.sender_protocol_address
       @topology.maybe_add_host(packet_in.source_mac,
                                packet_in.sender_protocol_address,
                                dpid,
